b46c3b782d2fb9146a633188c8a0995cd0c07ccd
DevLucasPr <lucas.saracho010@gmail.com>
2026-03-20 13:48:36 -0300

    agrego utils.h

--- src/main.cpp
+++ src/main.cpp
@@ -11,37 +11,37 @@
 #include <list>
 #include <map>
 
-std::list<Experiencia*> experiencias;
-std::map<std::string, Experiencia*> map_experiencias;
+std::list<Experiencia *> experiencias;
+std::map<std::string, Experiencia *> map_experiencias;
 
-std::list<Turista*> turistas;
-std::map<std::string, Turista*> map_turistas;
+std::list<Turista *> turistas;
+std::map<std::string, Turista *> map_turistas;
 
-void coleccion_guardarExperiencia(Experiencia* exp)
+void coleccion_guardarExperiencia(Experiencia *exp)
 {
 	experiencias.push_back(exp);
-	std::pair<std::string, Experiencia*> entry(exp->getCodigoReserva(), exp);
+	std::pair<std::string, Experiencia *> entry(exp->getCodigoReserva(), exp);
 	map_experiencias.insert(entry);
 }
-void coleccion_eliminarExperiencia(Experiencia* exp)
+void coleccion_eliminarExperiencia(Experiencia *exp)
 {
 	experiencias.remove(exp);
 	map_experiencias.erase(exp->getCodigoReserva());
 }
 
-void coleccion_guardarTurista(Turista* tur)
+void coleccion_guardarTurista(Turista *tur)
 {
 	turistas.push_back(tur);
-	std::pair<std::string, Turista*> entry(tur->getCi(), tur);
+	std::pair<std::string, Turista *> entry(tur->getCi(), tur);
 	map_turistas.insert(entry);
 }
 
-Turista* coleccion_getTurista(std::string ci)
+Turista *coleccion_getTurista(std::string ci)
 {
 	return map_turistas[ci];
 }
 
-Experiencia* coleccion_getExperiencia(std::string codigoReserva)
+Experiencia *coleccion_getExperiencia(std::string codigoReserva)
 {
 	return map_experiencias[codigoReserva];
 }
@@ -65,7 +65,6 @@ Experiencia* coleccion_getExperiencia(std::string codigoReserva)
 // cantNoches -> 15
 void parte_a()
 {
-
 }
 
 // Crear los siguientes objetos de la clase TourGuiado:
@@ -84,7 +83,6 @@ void parte_a()
 // lugaresVisitados -> Puerta de la Ciudadela, Mausoleo, Cabildo, Palacio Salvo
 void parte_b()
 {
-
 }
 
 // Crear los siguientes objetos de la clase EventoCultural:
@@ -96,14 +94,12 @@ void parte_b()
 // usoCupon -> true
 void parte_c()
 {
-
 }
 
-//d. Imprimir en consola utilizando la inserción de flujo el resultado de ejecutar la
-//operación getDT() para cada uno de los objetos Experiencia creados
+// d. Imprimir en consola utilizando la inserción de flujo el resultado de ejecutar la
+// operación getDT() para cada uno de los objetos Experiencia creados
 void parte_d()
 {
-
 }
 
 // Crear los siguientes objetos de la clase Turista:
@@ -111,92 +107,89 @@ void parte_d()
 // 1.535.442-0, Karen Santos, karen.s89@internet.uy
 void parte_e()
 {
-
 }
 
-//Imprimir en consola el resultado de ejecutar la operación toString para cada uno
-// de los objetos Turista creados
+// Imprimir en consola el resultado de ejecutar la operación toString para cada uno
+//  de los objetos Turista creados
 void parte_f()
 {
+	list<Turista *>::const_iterator iteradorTur;
+	for (iteradorTur = turistas.begin(); iteradorTur != turistas.end(); ++iteradorTur)
+	{
+		Turista *turista = *iteradorTur;
+		cout << turista->toString() << endl;
+	}
 
-}
+	// Registrar las siguientes relaciones entre turistas y experiencias (creando links de la
+	// relación en ambas direcciones):
+	// 4.951.278-9 (Vanesa Castro) -> ALX5489 (Hotel moderno)
+	// 4.951.278-9 (Vanesa Castro) -> ALJ4789 (Todas las habitaciones con vista al mar)
+	// 4.951.278-9 (Vanesa Castro) -> TGR3257 (Puntos emblematicos)
+	// 4.951.278-9 (Vanesa Castro) -> ECP1346 (Danza en el Solis)
+	// 1.535.442-0 (Karen Santos) -> TGO4657 (Plazas de Montevideo)
+	// 1.535.442-0 (Karen Santos) -> TGR3257 (Puntos emblematicos)
+	void parte_g()
+	{
+	}
 
-// Registrar las siguientes relaciones entre turistas y experiencias (creando links de la
-// relación en ambas direcciones):
-// 4.951.278-9 (Vanesa Castro) -> ALX5489 (Hotel moderno)
-// 4.951.278-9 (Vanesa Castro) -> ALJ4789 (Todas las habitaciones con vista al mar)
-// 4.951.278-9 (Vanesa Castro) -> TGR3257 (Puntos emblematicos)
-// 4.951.278-9 (Vanesa Castro) -> ECP1346 (Danza en el Solis)
-// 1.535.442-0 (Karen Santos) -> TGO4657 (Plazas de Montevideo)
-// 1.535.442-0 (Karen Santos) -> TGR3257 (Puntos emblematicos)
-void parte_g()
-{
+	// Invocar la operación listarExperiencias(10/12/2023, 0, 1000) para
+	//  la turista 4.951.278-9 (Vanesa Castro) e imprimir el resultado en consola (un string
+	//  por línea)
+	void parte_h()
+	{
+	}
 
-}
+	// Ejecutar la eliminación del objeto TGR3257(Puntos emblematicos) de la clase
+	// Experiencia
+	void parte_i()
+	{
+	}
 
-//Invocar la operación listarExperiencias(10/12/2023, 0, 1000) para
-// la turista 4.951.278-9 (Vanesa Castro) e imprimir el resultado en consola (un string
-// por línea)
-void parte_h()
-{
+	// Invocar la operación listarExperiencias(10/10/2020, 0, 1000)
+	// para la turista 1.535.442-0 (Karen Santos) e imprimir el resultado en consola (un
+	// string por línea)
+	void parte_j()
+	{
+	}
 
-}
+	// Imprimir en consola utilizando la inserción de flujo el resultado de ejecutar la
+	// operación getDT() para cada uno de los objetos Experiencias creados (mismo
+	// código que la parte d)
+	void parte_k()
+	{
+	}
 
-// Ejecutar la eliminación del objeto TGR3257(Puntos emblematicos) de la clase
-// Experiencia
-void parte_i()
-{
+	void cleanUp()
+	{
+	}
 
-}
+	int main()
+	{
+		std::cout << "parte_a" << std::endl;
+		parte_a();
+		std::cout << "parte_b" << std::endl;
+		parte_b();
+		std::cout << "parte_c" << std::endl;
+		parte_c();
+		std::cout << "parte_d" << std::endl;
+		parte_d();
+		std::cout << "parte_e" << std::endl;
+		parte_e();
+		std::cout << "parte_f" << std::endl;
+		parte_f();
+		std::cout << "parte_g" << std::endl;
+		parte_g();
+		std::cout << "parte_h" << std::endl;
+		parte_h();
+		std::cout << "parte_i" << std::endl;
+		parte_i();
+		std::cout << "parte_j" << std::endl;
+		parte_j();
+		std::cout << "parte_k" << std::endl;
+		parte_k();
+		std::cout << "cleanUp" << std::endl;
+		cleanUp();
+		std::cout << "fin" << std::endl;
 
-// Invocar la operación listarExperiencias(10/10/2020, 0, 1000)
-// para la turista 1.535.442-0 (Karen Santos) e imprimir el resultado en consola (un
-// string por línea)
-void parte_j()
-{
-
-}
-
-// Imprimir en consola utilizando la inserción de flujo el resultado de ejecutar la
-// operación getDT() para cada uno de los objetos Experiencias creados (mismo
-// código que la parte d)
-void parte_k()
-{
-
-} 
-
-void cleanUp()
-{
-
-}
-
-int main() 
-{
-	std::cout << "parte_a" <<  std::endl;
-	parte_a();
-	std::cout << "parte_b" <<  std::endl;
-	parte_b();
-	std::cout << "parte_c" <<  std::endl;
-	parte_c();
-	std::cout << "parte_d" <<  std::endl;
-	parte_d();
-	std::cout << "parte_e" <<  std::endl;
-	parte_e();
-	std::cout << "parte_f" <<  std::endl;
-	parte_f();
-	std::cout << "parte_g" <<  std::endl;
-	parte_g();
-	std::cout << "parte_h" <<  std::endl;
-	parte_h();
-	std::cout << "parte_i" <<  std::endl;
-	parte_i();
-	std::cout << "parte_j" <<  std::endl;
-	parte_j();
-	std::cout << "parte_k" <<  std::endl;
-	parte_k();
-	std::cout << "cleanUp" <<  std::endl;
-	cleanUp();
-	std::cout << "fin" <<  std::endl;
-
-	return 0;
-}
+		return 0;
+	}
